126255c3984f4e47d2f1329fd8d92817ee7ca63d
DFSWeb <contato@dfsweb.com.br>
2020-01-12 22:43:33 -0300

    Modificando rotas para acesso as páginas criadas

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+//Mudando nosso default
+$route['default_controller'] = 'pages/view';
+//Para acessar o welcome agora através deste route
+//$route['welcome'] = 'welcome/index';
+//Expressão para acessa as páginas pra qualquer coisa aplica-se essa regra para
+//o primeiro item acessado pela url
+$route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
