e91dc83478b356f742e4fa12134221bb3b92ce84
DFSWeb <contato@dfsweb.com.br>
2020-01-12 23:19:00 -0300

    Criando model e fazendo as consultas de banco

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -1,6 +1,21 @@
 <?php 
 class News_model extends CI_Model{
-    function function_name() {
-        ;
+    //Funcão que carrega as configurações para acesso ao banco de dados
+    public function __construct(){
+        $this->load->database();
+    }
+    
+    //buscar listas de noticias
+    public function get_news($uri = FALSE){
+        //Verifica se uri é false senão traz a noticia específica
+        if($uri === FALSE){
+            //Buscando a lista de noticias
+            $query = $this->db->get('news');
+            return $query->result_array();
+        } else {
+            //Buscando uma única noticia
+            $query = $this->db->get_where('news', array('uri'=>$uri));
+            return $query->row_array();
+        }
     }
 }
